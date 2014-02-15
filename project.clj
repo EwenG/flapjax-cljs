d20050f40ef2839caf188ad96c257a4e9120556a
ewen <ewen.grosjean@gmail.com>
2014-02-15 20:07:18 +0100

    SNAPSHOT version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ewen/flapjax-cljs "1.0.1"
+(defproject ewen/flapjax-cljs "1.0.1-SNAPSHOT"
   :description "Utilities to use flapjax from clojurescript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
